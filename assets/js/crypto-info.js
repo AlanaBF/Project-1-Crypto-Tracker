c71ea04800005a64e4ea7f62afd1767702bb2f3f
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-02-01 23:23:39 +0000

    I have added comments to crypto-info.js containing short todo list and the basic written layout of the extra info card which the function will render to the screen

---
+++ assets/js/crypto-info.js
@@ -0,0 +1,16 @@
+// TODO: Get list of what extra info I want to have about the specific coin
+
+// TODO: Write function that takes as parameters the response objects/json then creates card that will contains all the information.
+
+// TODO: Add functionality which takes the raw data, parses it and builds a section containing all of that data nicely arranged and styled ready to be used.
+
+// this is basic lay out of the additional info card
+//! coin info:
+//!     {COIN} Value Statistics
+//!     an overview showing the statistics of the {COIN}
+//!
+//!     {icon} Price to USD - ${PRICE}
+//!     {icon} Rank - {RANK}
+//!     {icon} 24h Volume - {VOLUME}
+//!     {icon} Market Cap - {MARKETCAP}
+//!     {icon} All-Time-High(daily avg.) - {HIGH-DAILY-AVG}
