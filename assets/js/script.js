2acbd29b0e1864f1d9a1db4e7e5f57917465a661
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-02-01 20:55:59 +0000

    js code added with extra functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
+async function getListOfAllCoins() {
+	//Get a list of coins 
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
+		"method": "GET",
+		"headers": {
+			"X-RapidAPI-Key": "db60a21bf6msh0c7c9e1851c4408p1d3b11jsnca09255a288c",
+			"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+		}
+	};
+
+	const response = await $.ajax(settings).then(function (response) {
+		return response
+        console.log(response);
+	})
+    return response
+    
+}
+
 
 function createCoinCard(name, price, change) {
-    const coinCard = `
+    const coinCard = 
+    `
     <div class="card">
     <h2>Currency ${name}</h2>
     <p>Price $${price}</p>
@@ -8,16 +30,21 @@ function createCoinCard(name, price, change) {
     </div>
     `
 return coinCard;
+}
+
+function renderCoinCards(coins) {
+    
+coins.forEach(function(coin) {
+const coinCardEl = createCoinCard(coin.name, coin.price, coin.change)
+$("#top10").append(coinCardEl)
+
+})
+
 
 }
 
-
-function renderCoinCards() {
-    for (var i=0; i < coinCard.length; i++) {
-
-    }
-}
-
+var response = getListOfAllCoins();
+console.log(response);
 
 
 
---
+++ assets/services/coins.js
@@ -0,0 +1,37 @@
+function getListOfCoins() {
+	//Get a list of coins 
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
+		"method": "GET",
+		"headers": {
+			"X-RapidAPI-Key": "db60a21bf6msh0c7c9e1851c4408p1d3b11jsnca09255a288c",
+			"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+		}
+	};
+
+	return $.ajax(settings).then(function (response) {
+		console.log(response);
+	})
+}
+
+//Get specific coin by its UUID
+function getOneCoin() {
+var UUID = '';
+const parameters = {
+	"async": true,
+	"crossDomain": true,
+	"url": `https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd?referenceCurrencyUuid=${UUID}&timePeriod=24h`,
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "db60a21bf6msh0c7c9e1851c4408p1d3b11jsnca09255a288c",
+		"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+	}
+};
+
+$.ajax(parameters).done(function (response) {
+	console.log(response);
+});
+}
+getListOfCoins();
