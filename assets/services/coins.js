102d258255149eab20158f1befc80faea8620e0b
Ainhoa Prada <apt.code14@gmail.com>
2023-02-01 23:13:17 +0100

    API COINS coin price history added

--- assets/services/coins.js
+++ assets/services/coins.js
@@ -35,3 +35,22 @@ $.ajax(parameters).done(function (response) {
 });
 }
 getListOfCoins();
+
+//Get iformation for the chart
+function getCoinPriceHistory () {
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h",
+		"method": "GET",
+		"headers": {
+			"X-RapidAPI-Key": "db60a21bf6msh0c7c9e1851c4408p1d3b11jsnca09255a288c",
+			"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+		}
+	};
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+}
+
