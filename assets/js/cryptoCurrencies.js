993cb349704548f8337843f00ad49ea266bf8acf
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-02-02 20:12:22 +0000

    Search started

--- assets/js/cryptoCurrencies.js
+++ assets/js/cryptoCurrencies.js
@@ -1,4 +1,4 @@
-async function displayCoins() {
+async function displayAllCoins() {
 	try {
 		// Get data from API by calling the getListOfCoins() function
 		const response = await getListOfCoins(100);
@@ -10,41 +10,8 @@ async function displayCoins() {
 	}
 }
 
+displayAllCoins();
 
-// Function to create a single coin card with the given coin data
-function createCoinCard(coin) {
-	// Destructure variables from the response
-	const { name, price, change, iconUrl, symbol, uuid } = coin;
-
-	// Return the HTML template for a single coin card
-	return `
-    <div data-uuid='${uuid}' class="card">
-      <div class='card-header'>
-        <h2>${name}</h2>
-        <img class='coin-icon' width='30px' src='${iconUrl}' alt='${symbol} icon'/>
-      </div>
-      <p>Price $${price}</p>
-      <p>Daily Change ${change}%</p>
-    </div>
-  `;
-}
-
-
-// Function to render 100 coin cards. Takes an array of coins and appends each coin card to coins wrapper and then to the given parent element
-function renderCoinCards(coins, parentElement) {
-	//Create wrapper container for all cards
-	const cardsWrapperEl = $('<div>');
-
-	// Loop through the array of all coins and create a card for each coin
-	coins.forEach(function (coin) {
-		const coinCard = createCoinCard(coin);
-		// Append the coin card to the cards wrapper
-		cardsWrapperEl.append(coinCard);
-	});
-
-	// Append the cardsWrapperEl to the parent element
-	parentElement.append(cardsWrapperEl);
-}
-
-// Call the displayCoins() function to start the process
-displayCoins();
+$('seacrh-form').on('submit', function(e){
+  e.preventDefault();
+});
