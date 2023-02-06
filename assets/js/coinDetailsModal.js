80c37c82f4a487413dac33cfb88e3d5fe324da53
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-02-06 12:20:25 +0000

    coinDetailModal content generated

--- assets/js/coinDetailsModal.js
+++ assets/js/coinDetailsModal.js
@@ -14,46 +14,89 @@ async function displayCoinDetails() {
 
 }
 
-displayCoinDetails();
-
 function createCoinModal(coin) {
-  const { name, symbol, price, rank, marketCap, allTimeHigh } = coin;
+  const { name, symbol, description } = coin;
   let formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
   const coinModal = `
-<div class="coin-modal" id="coinModal">
-    <div class="coin-modal-inner" id="coinModalInner">
-        <div class="coin-modal-header">
-            <h2>${name} (${symbol}) </h2>
-            <p>
-            ${name} live price in US Dollars. View value statistic, market cap and supply.
-            </p>
-           <div class="chart-wrapper">
-            <h4>${name} Price History</h4>
-            <canvas role='img' id="chart"></canvas>
-           </div>
-        </div>
-        <div class="coin-modal-info">
-            <div class="statistics-block">
-              <p>An overview showing the statistics of the Bitcoin</p>
-              <p>Price to USD: $${Number(price).toFixed(2)}</p>
-              <p>Rank: ${rank}</p>
-              <p>24h Volume: $${formatter.format(coin['24hVolume'])}</p>
-              <p>Market cap: $${formatter.format(marketCap)}</p>
-              <p>All-time-high(daily avg.): $${Number(allTimeHigh.price).toFixed(2)}</p>
+  <div class="coin-modal" id="coinModal">
+      <div class="coin-modal-inner" id="coinModalInner">
+          <div class="coin-modal-header">
+              <h1>${name} (${symbol}) </h1>
+              <p>
+              ${name} live price in US Dollars. View value statistic, market cap and supply.
+              </p>
+            <div class="chart-wrapper">
+              <h4>${name} Price History</h4>
+              <canvas role='img' id="chart"></canvas>
             </div>
-            <div class="statistics-block">
-              <p>An overview showing the statistics of the Bitcoin</p>
-              <p>Price to USD: $${Number(price).toFixed(2)}</p>
-              <p>Rank: ${rank}</p>
-              <p>24h Volume: $${formatter.format(coin['24hVolume'])}</p>
-              <p>Market cap: $${formatter.format(marketCap)}</p>
-              <p>All-time-high(daily avg.): $${Number(allTimeHigh.price).toFixed(2)}</p>
+          </div>
+          <div id='modalInfo' class="coin-modal-info">
+          ${createCoinStatsBlock(coin)}
+          ${createOtherStatsBlock(coin)}
+              <div class="statistics-block">
+                <h2> What is ${name}</h2>
+                ${description}
+              </div>
+              <div class="statistics-block">
+              <h2> ${name} Links</h2>
+            ${createCoinLinks(coin.links)}
             </div>
-        </div>
-    </div>
-</div>
+          </div>
+      </div>
+  </div>
 `
   return coinModal;
 }
 
+
+function createCoinStatsBlock(coin) {
+  const { name, price, rank, marketCap, allTimeHigh } = coin;
+
+  return `
+    <div class="statistics-block">
+      <h2> ${name} Value Statisstics</h2>
+      <span class='subtitle'>An overview showing the statistics of the Bitcoin</span>
+      <div class='info'><p>Price to USD:</p> <span>$${Number(price).toFixed(2)}</span></div>
+      <div class='info'><p>Rank: </p> <span>${rank}</span></div>
+      <div class='info'><p>24h Volume:</p> <span>$${formatter.format(coin['24hVolume'])}</span></div>
+      <div class='info'><p>Market cap:</p> <span>$${formatter.format(marketCap)}</span></div>
+      <div class='info'><p>All-time-high(daily avg.):</p> <span>$${Number(allTimeHigh.price).toFixed(2)}</span></div>
+    </div>
+`
+}
+
+function createOtherStatsBlock(coin) {
+  const { numberOfMarkets, numberOfExchanges, supply } = coin;
+
+  return `
+    <div class="statistics-block">
+      <h2> Other Statisstics</h2>
+      <span class='subtitle'>An overview showing the statistics of the Bitcoin</span>
+      <div class='info'><p>Number Of Markets:</p> <span>${numberOfMarkets}</span></div>
+      <div class='info'><p>Number Of Exchanges: </p> <span>${numberOfExchanges}</span></div>
+      <div class='info'><p>Aprroved Supply:</p> <span>${supply.confirmed ? 'Yes' : 'No'}</span></div>
+      <div class='info'><p>Market cap:</p> <span>$${formatter.format(supply.total)}</span></div>
+      <div class='info'><p>Max Supply:</p> <span>$${formatter.format(supply.max)}</span></div>
+    </div>
+    `
+}
+
+function createCoinLinks(links) {
+  let comb = '';
+
+  links.forEach(link => {
+    const { type, url, name } = link;
+
+    comb += `
+        <div class='info'>
+          <p>${type}</p>
+          <a href='${url}' target='_blank ref='no-referrer'>${name}</a>
+        </div>
+        `
+  });
+  return comb;
+}
+
+
+displayCoinDetails();
